8c02b273c4c2ec7fcedf305670e79ccd4815dfd6
Chad Williams <fadewave@gmail.com>
2018-02-21 16:56:10 -0800

    add search bar and table view. Search uses fake results.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Chad on 2/21/18.
+//  Copyright © 2018 Chad Williams. All rights reserved.
+//
+
+// import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}

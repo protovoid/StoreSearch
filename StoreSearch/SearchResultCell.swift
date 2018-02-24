ab51bdec3dcbfd428ae4b7141a16c7573f830953
Chad Williams <fadewave@gmail.com>
2018-02-23 16:09:27 -0800

    use custom cells for search results

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Chad on 2/23/18.
+//  Copyright © 2018 Chad Williams. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+  
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistNameLabel: UILabel!
+  @IBOutlet weak var artworkImageVIew: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -13,12 +13,23 @@ class SearchViewController: UIViewController {
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var tableView: UITableView!
   
+  struct TableViewCellIdentifiers {
+    static let searchResultCell = "SearchResultCell"
+    static let nothingFoundCell = "NothingFoundCell"
+  }
+  
   var searchResults = [SearchResult]()
   var hasSearched = false
 
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    tableView.rowHeight = 80
+    
+    var cellNib = UINib(nibName: TableViewCellIdentifiers.searchResultCell, bundle: nil)
+    tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.searchResultCell)
+    cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell, bundle: nil)
+    tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
   }
 
   override func didReceiveMemoryWarning() {
@@ -36,7 +47,7 @@ extension SearchViewController: UISearchBarDelegate {
     if searchBar.text! != "justin bieber" {
       for i in 0...2 {
         let searchResult = SearchResult()
-        searchResult.name = String(format: "Fake Result %d for '%@'", i)
+        searchResult.name = String(format: "Fake Result %d for", i)
         searchResult.artistName = searchBar.text!
         searchResults.append(searchResult)
       }
@@ -62,21 +73,16 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cellIdentifier = "SearchResultCell"
     
-    var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-    if cell == nil {
-      cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-    }
     if searchResults.count == 0 {
-      cell.textLabel!.text = "(Nothing found)"
-      cell.detailTextLabel!.text = ""
+      return tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifiers.nothingFoundCell, for: indexPath)
     } else {
+      let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
       let searchResult = searchResults[indexPath.row]
-      cell.textLabel!.text = searchResult.name
-      cell.detailTextLabel!.text = searchResult.artistName
+      cell.nameLabel.text = searchResult.name
+      cell.artistNameLabel.text = searchResult.artistName
+      return cell
     }
-    return cell
   }
   
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
